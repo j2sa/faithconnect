c7ff486d1477ab22c372c186ab12aa6b6a55ac6b
j2sa <joel.martinez@j2sa.tec.br>
2024-10-31 17:06:44 -0300

    alterado para utilizar bearer token

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,11 +2,14 @@ const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
   try {
-    // Buscar o token nos cookies
-    const token = req.cookies.accessToken;
+    // Buscar o token no cabeçalho de autorização
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
     if (!token) {
       return res.status(401).send('Acesso negado. Token não fornecido.');
     }
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
