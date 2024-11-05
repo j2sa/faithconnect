83f907ac3e8ad02c7eb9e482bd537a70eb8492ab
j2sa <joel.martinez@j2sa.tec.br>
2024-11-05 15:36:08 -0300

    vercel

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const publicRoutes = require('./routes/publicRoutes');
 const auth = require('./middleware/auth');
 const axios = require('axios');
 
-dotenv.config({ path: '../.env' });
+dotenv.config({ path: '.env' });
 
 const app = express();
 const PORT = process.env.BACKEND_PORT;
@@ -42,28 +42,4 @@ app.get('/', (req, res) => {
   res.send('ChurchHub API');
 });
 
-/*
-// Add a new route for fetching CEP data
-app.get('/api/cep/:cep', async (req, res) => {
-  const { cep } = req.params;
-
-  try {
-    const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
-    const data = response.data;
-
-    res.json({
-      cep: data.cep,
-      logradouro: data.logradouro,
-      complemento: data.complemento,
-      bairro: data.bairro,
-      localidade: data.localidade,
-      uf: data.uf,
-    });
-  } catch (error) {
-    res.status(error.status).json({ code: error.code });
-  }
-});
-*/
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
